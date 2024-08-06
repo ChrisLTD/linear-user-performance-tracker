fc8869654f022b0dbac58cea2d7bc30bedbb3e18
Chris Johnson <chris@getparker.com>
2024-08-06 15:37:28 -0400

    fixed script

--- get-ids.js
+++ get-ids.js
@@ -6,14 +6,14 @@ const apiKey = process.env.API_KEY;
 
 const headers = {
     'Content-Type': 'application/json',
-    'Authorization': 'Bearer ' + apiKey,
+    'Authorization': apiKey,
 };
 
 async function fetchFromLinear(query) {
     const response = await fetch('https://api.linear.app/graphql', {
         method: 'POST',
         headers: headers,
-        body: JSON.stringify({ query: query }),
+        body: JSON.stringify({ query: query, variables: {} }),
     });
 
     const json = await response.json();
@@ -32,17 +32,18 @@ async function listUsers() {
         }
     `;
 
-    const users = await fetchFromLinear(usersQuery);
+    const response = await fetchFromLinear(usersQuery);
+    const users = response.users;
 
     // Sort teams and users by name
-    users.users.nodes.sort((a, b) => a.name.localeCompare(b.name));
+    users.nodes.sort((a, b) => a.name.localeCompare(b.name));
 
     // Find the longest name
-    const allNames = [...users.users.nodes.map(n => n.name)];
+    const allNames = [...users.nodes.map(n => n.name)];
     const longestNameLength = allNames.reduce((max, name) => Math.max(max, name.length), 0);
 
     console.log("\nUsers:");
-    users.users.nodes.forEach(user => console.log(`${user.name.padEnd(longestNameLength)} id: ${user.id}`));
+    users.nodes.forEach(user => console.log(`${user.name.padEnd(longestNameLength)} id: ${user.id}`));
 }
 
 listUsers();
